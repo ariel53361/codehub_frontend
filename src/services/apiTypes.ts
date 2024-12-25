1bf21ddd5b312f1f9799ca839b476351cffa26e6
ariel53361 <ariel53361@gmail.com>
2024-12-25 15:45:31 +0200

    deleting MutationError interface

--- src/services/apiTypes.ts
+++ src/services/apiTypes.ts
@@ -1,5 +1,3 @@
-import { AxiosError } from "axios";
-
 export interface FetchResponse<T> {
   count: number;
   next: string | null;
@@ -8,8 +6,4 @@ export interface FetchResponse<T> {
 
 export interface ApiError {
   [key: string]: string[];
-}
-
-export interface MutationError extends AxiosError<ApiError> {
-  messages?: string[];
 }
